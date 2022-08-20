884f17b27970c28667637e7e1a515ad73019fe9f
Pablo Spector <pablo_spector@hotmail.com>
2022-08-20 00:12:13 -0300

    nuevos ajustes database

--- src/lib/initialfunction.js
+++ src/lib/initialfunction.js
@@ -4,9 +4,9 @@ const data = require('./data.json')
 exports.fillDatabase = async () => {
     try {
         var count = await Product.estimatedDocumentCount()
-        console.log("db count", count);
+        
         if (count > 0) return
-        console.log("data length", data.length);
+        
         await Product.insertMany(data)
     
     
