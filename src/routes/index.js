7f8b3e0d73b80f13e870de0387768e3bdd9caca9
Pablo Spector <40707996+PabloElectricista@users.noreply.github.com>
2022-08-23 04:25:27 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,9 +6,10 @@ const usersRoutes = require('./users');
 
 const filtersRoutes = require('./filters')
 
+router.get('/', (req, res)=> res.send('Hello')
+
 /* products routes */
 router.use('/products', productsRoutes);
-
 
 /* users routes */
 router.use('/users', usersRoutes);
@@ -16,6 +17,4 @@ router.use('/users', usersRoutes);
 /* filters routes*/
 router.use('/filter',filtersRoutes)
 
-
 module.exports = router;
-
