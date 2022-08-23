561e6b6887dce1a1fec23b31a42c5c8370f883d6
Pablo Spector <40707996+PabloElectricista@users.noreply.github.com>
2022-08-23 05:07:59 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ fillDatabase();
 
 app.use(logger('dev'));
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", 'http://localhost:3000'); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", 'http://200.81.120.99'); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Credentials", "true");
     res.header(
         "Access-Control-Allow-Headers",
