d6ed0c273710a84bd55a517fca5adceac1ce6446
Pablo Spector <40707996+PabloElectricista@users.noreply.github.com>
2022-08-24 08:40:22 -0300

    Update Products.js

--- src/models/Products.js
+++ src/models/Products.js
@@ -8,11 +8,8 @@ const ProductSchema = new mongoose.Schema(
         { type: String, required: true },
         image: 
         [{ type: String, required: true }],
-        categorie: 
-        img: 
-        { type: String, required: true },
         category: 
-        { type: Array},
+        [{ type: String, required: true }],
         color: 
         { type: String, required: true },
         price: 
