f519375089bd0732f31215c4e073478b7f5a6faa
roger077 <roger.rm03@gmail.com>
2022-08-24 12:47:16 -0300

    cloudinary in back

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const productsRoutes = require('./products');
 const usersRoutes = require('./users');
 const filtersRoutes = require('./filters')
+const imagesRoutes=require('./images')
 const {postImgCloudinary}=require('../controlers/cloudinary/cloudinary');
 /* products routes */
 router.use('/products', productsRoutes);
@@ -13,6 +14,6 @@ router.use('/users', usersRoutes);
 /* filters routes*/
 router.use('/filter',filtersRoutes)
 
-router.post('/img',upload.array('image',4),postImgCloudinary)
+router.use('/img',imagesRoutes)
 module.exports = router;
 
