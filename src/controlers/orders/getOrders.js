18432288a729e39ee3beab9c138aedc164114b2a
Pablo Spector <40707996+PabloElectricista@users.noreply.github.com>
2022-08-28 16:37:19 -0300

    Update getOrders.js

--- src/controlers/orders/getOrders.js
+++ src/controlers/orders/getOrders.js
@@ -2,7 +2,7 @@ const Order = require("../../models/Order");
 
 const getOrders = async (req, res, next) => {
     try {
-        const orders = await Order.find({}).populate(["user", "products"])
+        const orders = await Order.find({}).populate({ path: "user products userseller"})
         console.log(orders);
         res.json(orders)
     } catch (error) {
