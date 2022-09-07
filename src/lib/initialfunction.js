83d6871cf07648ff576bd6ee00c1600d6b17a1db
Pablo Spector <pablo_spector@hotmail.com>
2022-09-07 17:00:26 -0300

    stripe

--- src/lib/initialfunction.js
+++ src/lib/initialfunction.js
@@ -2,6 +2,9 @@ const Product = require("../models/Products");
 const data = require('./data.json')
 
 exports.fillDatabase = async () => {
+    for (const item of data) {
+        item.user = "6318c0d8409bf52757b70bc9"
+    }
     try {
         var count = await Product.estimatedDocumentCount()
         
