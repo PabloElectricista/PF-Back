54d2948931b6b73c885133ff5e703e49063faa5e
Pablo Spector <pablo_spector@hotmail.com>
2022-09-08 07:00:30 -0300

    ordenes

--- src/controlers/orders/getOrders.js
+++ src/controlers/orders/getOrders.js
@@ -1,10 +1,16 @@
 const Order = require("../../models/Order");
+const Product = require("../../models/Products");
 
 const getOrders = async (req, res) => {
 
     try {
         const orders = await Order.find({}).
-            populate({ path: "user products userseller"})
+            populate({ path: "user userseller"})
+        for (const order of orders) {
+            for (let prod of order.products) {
+                prod.products = await Product.findById(prod.products)
+            }
+        }
         res.json(orders)
     } catch (error) {
         res.send({ error: error.message });
